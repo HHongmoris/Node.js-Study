c8e7a86d864d860904e5d0b51445bc1e327440d5
HHongmoris <moris8470@gmail.com>
2024-01-24 16:08:37 +0900

    0124 node study

--- Ch03/05-refactoring-router.js
+++ Ch03/05-refactoring-router.js
@@ -6,18 +6,24 @@ http.createServer((req, res)=> {
     //path명 할당
     const path = url.parse(req.url, true).pathname;
     res.setHeader("Content-Type", "text/html");
-
-    if(path === "/user"){
-        //user 함수 실행
-        user(req, res);
-    }else if(path === "/feed"){
-        // feed 함수 실행
-        feed(req, res);
+    // if(path === "/user"){
+    //     //user 함수 실행
+    //     user(req, res);
+    // }else if(path === "/feed"){
+    //     // feed 함수 실행
+    //     feed(req, res);
+    // }else{
+    //     // notFound 함수 실행
+    //     notFound(req, res);
+    // }
+    // urlMap에 path가 있는지 확인
+    if(path in urlMap){
+        //urlMap에 path값으로 매핑된 함수 실행
+        urlMap[path](req, res);
     }else{
-        // notFound 함수 실행
         notFound(req, res);
     }
-}).listen("3000", ()=> console.log("라우터를 만들어보자!"));
+}).listen("3000", ()=> console.log("라우터 리펙터링!"));
 
 
 const feed = (req, res) => {
@@ -39,4 +45,11 @@ const user = (req, res) => {
     //쿼리 스트링 데이터를 userInfo에 할당
     res.end(`[user] name: ${userInfo.name}, age: ${userInfo.age}`);
     //결과값으로 이름과 나이 설정
+};
+
+// 라우터 규칙 매핑 키로 path가 들어가고 값에 함수를 할당
+const urlMap = {
+    "/": (req, res) => res.end("HOME"),
+    "/user": user,
+    "/feed": feed,
 };
