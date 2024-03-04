d6783e5f3f17552540784b777f16d41b1dca9a6f
Sungmin-Hong <moris8470@gmail.com>
2024-03-05 01:32:01 +0900

    0305 nestjs mongodb

--- Ch08/blog/src/app.module.ts
+++ Ch08/blog/src/app.module.ts
@@ -2,11 +2,18 @@
 import { Module } from "@nestjs/common";
 import { BlogController } from "./blog.controller";
 import { BlogService } from "./blog.service";
-import { BlogFileRepository } from "./blog.repository";
+import { BlogFileRepository, BlogMongoRepository } from "./blog.repository";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Blog, BlogSchema } from "./blog.schema";
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      "mongodb+srv://<아이디>:<패스워드>@<클러스터정보>/blog",
+    ),
+    MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
+  ],
   controllers: [BlogController],
-  providers: [BlogService, BlogFileRepository],
+  providers: [BlogService, BlogFileRepository, BlogMongoRepository],
 })
 export class AppModule {}
